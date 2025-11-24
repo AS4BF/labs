13e41948ff67cc58fbbc397a3ecab7c319adc2ca
AS4BF <andrey00rt@gmail.com>
2025-11-24 20:28:48 +0300

    interface team

--- Team/include/team.h
+++ Team/include/team.h
@@ -1,44 +1,48 @@
 #ifndef TEAM_H
 #define TEAM_H
 #include <string>
+#include <functional>
+#include <format>
 #include "INameAndCopy.h"
 #include "to_string.h"
 #include "equals.h"
+#include "hash.h"
 
 namespace variant3{
 using std::string;
 
-class Team : public variant3::INameAndCopy<Team>, Share::Equals<Team>, Share::ToString {
+class Team : public variant3::INameAndCopy<Team>, Share::Equals<Team>, Share::ToString, Share::Hash {
 protected:
 	unsigned int rnum_;
 public:
-	Team(const string& name, const int& rnum); //: INameAndCopy(name), rnum_{rnum} {};
+	Team(const string& name, const unsigned int& rnum); //: INameAndCopy(name), rnum_{rnum} {};
 	Team();
 	
-	const int& 
-	get_rnum();	
+	const unsigned int& 
+	get_rnum() const noexcept;	
 
 	void 
-	set_rnum(const unsigned int& rnum);
-	
+	set_rnum(const unsigned int& rnum) noexcept;
+
 	Team 
 	DeepCopy() const override ;
 
 	bool
-	equals(const Team& rhs) const override ;
+	equals(const Team& rhs) const override noexcept;
 
 	bool 
-	operator==(const Team& rhs) const ;
+	operator==(const Team& rhs) const noexcept;
 
 	bool 
-	operator!=(const Team& rhs) const ;
+	operator!=(const Team& rhs) const noexcept;
 
 	int 
 	GetHashCode() const override ;
 
 	string
 	ToString() const override ;
-
+	
+	virtual ~Team();
 };
 
 };
